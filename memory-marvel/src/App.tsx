887ea2c073ade456a3e263b1018de3e5e07c8e58
Ryan Saffer <ryansaffer@gmail.com>
2024-12-04 08:53:52 +1100

    step 1

--- memory-marvel/src/App.tsx
+++ memory-marvel/src/App.tsx
@@ -4,16 +4,19 @@ import GameInfo from './components/GameInfo/GameInfo'
 import GameModal from './components/GameModal/GameModal'
 import Header from './components/Header/Header'
 import shapes from './data'
+import { generateCards } from './utils/generate-cards'
 
 function App() {
   const showModal = false
+
+  const cards = generateCards(shapes)
 
   return (
     <div>
       <Header />
       <GameInfo moves={0} score={0} timer={'60'} />
       <div className="cards-container">
-        {shapes.map(({ shapeId, uniqueId, shape }) => (
+        {cards.map(({ shapeId, uniqueId, shape }) => (
           <Card
             key={uniqueId}
             uniqueId={uniqueId}
---
+++ memory-marvel/src/utils/generate-cards.ts
@@ -0,0 +1,32 @@
+import { Shape } from '../data'
+
+export function generateCards(shapes: Shape[]) {
+  // duplicate the cards
+  const duplicates: Shape[] = []
+  shapes.forEach((shape) => {
+    duplicates.push(shape)
+    duplicates.push(shape)
+  })
+
+  // shuffle the cards
+  return shuffle(duplicates)
+}
+
+function shuffle(shapes: Shape[]) {
+  let currentIndex = shapes.length
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    const randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
+
+    // And swap it with the current element.
+    ;[shapes[currentIndex], shapes[randomIndex]] = [
+      shapes[randomIndex],
+      shapes[currentIndex],
+    ]
+  }
+
+  return shapes
+}
